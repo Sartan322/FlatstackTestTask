ddbd58953d0e8214cb64ef7e2165ff25800893a5
Pavel Prokopev <Sartanlytik88@gmail.com>
2021-06-12 18:01:13 +0300

    Initial Commit

---
+++ FlatstackTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatstackTestTask
+//
+//  Created by Павел Прокопьев on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
